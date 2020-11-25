d220033f3084cf25f33eeaaa9ff44ce59a4697b9
Joao Santana <joao.santana@sensedia.com>
2020-11-25 09:37:45 -0300

    logging messages

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelloWorld {
+
+  static final Logger log = Logger.getLogger(HelloWorld.class.getName());
+
   public static void main(String args[]) {
+
+    log.info("info message...");
+    log.warning("warning message...");
+    log.severe("severe message...");
+    log.config("config message...");
+
     System.out.println("Hello world");
   }
 }
